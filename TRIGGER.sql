9272c4f685fb9757f7fc2590ca1b0f8f8f8d718e
Sara López Marín <slopezm13@eafit.edu.co>
2024-09-26 12:52:29 -0500

    Update TRIGGER.sql

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -17,7 +17,7 @@ BEGIN
     -- Inserta un nuevo registro en la tabla `hueco` cada vez que se inserta un cliente.
     -- Se insertan valores en varias columnas de la tabla `hueco`. 
     -- '1' es el ID de la funeraria, y 'SI' indica que el hueco está ocupado.
-    -- Los demás valores, como el tipo de hueco, la ubicación y las medidas, se dejan como NULL (sin valor).
+    -- Los demás valores, como el tipo de hueco, la ubicación y las medidas, se modifican siempre con los datos que se deseen cambiar).
     -- `NEW.id_cliente` se refiere al ID del cliente recién insertado y lo asigna a la columna `cliente_id_cliente` de la tabla `hueco`.
     INSERT INTO hueco(funeraria_id_funeraria, tipo_hueco, ocupado, ubicacion, medidas, cliente_id_cliente)
     VALUES (1, NULL, 'SI', NULL, NULL, NEW.id_cliente);
